3b126a04d670c00cdbd23ef1cd939c42736efebd
Yyooogurt <ryaboshapka_egor@icloud.com>
2023-10-05 19:13:40 +0300

    Added styles

--- src/pages/Default.jsx
+++ src/pages/Default.jsx
@@ -9,47 +9,219 @@ function Default() {
             <div className="default-container">
               <div className="default-block">
                 <div className="default-title">Typography</div>
-                <h1 className="title title-primary">Heading 1</h1>
-                <h2 className="title title-secondary">Heading 2</h2>
-                <h3 className="title title-tertiary">Heading 3</h3>
-                <h4 className="title title-quaternary">Heading 4</h4>
-                <h5 className="title title-fivefold">Heading 5</h5>
-                <h6 className="title title-sixfold">Heading 6</h6>
+                <div className="default-block__wrap">
+                  <ul className="default-list">
+                    <span className="default-span">Play</span>
+                    <li>
+                      <h1 className="title title-primary">Heading 1</h1>
+                    </li>
+                    <li>
+                      <h2 className="title title-secondary">Heading 2</h2>
+                    </li>
+                    <li>
+                      <h3 className="title title-tertiary">Heading 3</h3>
+                    </li>
+                    <li>
+                      <h4 className="title title-quaternary">Heading 4</h4>
+                    </li>
+                    <li>
+                      <h5 className="title title-fivefold">Heading 5</h5>
+                    </li>
+                    <li>
+                      <h6 className="title title-sixfold">Heading 6</h6>
+                    </li>
+                  </ul>
+                  <ul className="default-list">
+                    <span className="default-span">Inter</span>
+                    <li>
+                      <h1 className="title title-primary">Heading 1</h1>
+                    </li>
+                  </ul>
+                </div>
               </div>
               <div className="default-block">
                 <div className="default-title">Buttons</div>
-                <button className="button button-primary" type="button">
-                  Sell a House
-                </button>
-                <button className="button button-primary disable" type="button">
-                  Sell a House
-                </button>
-                <button className="button button-secondary" type="button">
-                  Sell a House
-                </button>
-                <button
-                  className="button button-secondary disable"
-                  type="button"
-                >
-                  Sell a House
-                </button>
+                <div className="default-block__wrap">
+                  <ul className="default-list">
+                    <span className="default-span">Active</span>
+                    <li>
+                      <button className="button button-primary" type="button">
+                        Sell a House
+                      </button>
+                    </li>
+                    <li>
+                      <button className="button button-secondary" type="button">
+                        Sell a House
+                      </button>
+                    </li>
+                  </ul>
+                  <ul className="default-list">
+                    <span className="default-span">Disabled</span>
+                    <li>
+                      <button
+                        className="button button-primary disable"
+                        type="button"
+                      >
+                        Sell a House
+                      </button>
+                    </li>
+                    <li>
+                      <button
+                        className="button button-secondary disable"
+                        type="button"
+                      >
+                        Sell a House
+                      </button>
+                    </li>
+                  </ul>
+                </div>
               </div>
               <div className="default-block">
                 <div className="default-title">Links</div>
-                <a href="-" className="link">
-                  Home Sales
-                </a>
-                <a href="-" className="link link__arrow">
-                  Home Sales
-                  <LinkArrow />
-                </a>
-                <a href="-" className="link link__arrow disable">
-                  Home Sales
-                  <LinkArrow />
-                </a>
+                <div className="default-block__wrap">
+                  <ul className="default-list">
+                    <span className="default-span">Primary active</span>
+                    <li>
+                      <a href="-" className="link">
+                        Home Sales
+                      </a>
+                    </li>
+                  </ul>
+                  <ul className="default-list">
+                    <span className="default-span">
+                      Primary active with icon
+                    </span>
+                    <li>
+                      <a href="-" className="link link__arrow">
+                        Home Sales
+                        <LinkArrow />
+                      </a>
+                    </li>
+                  </ul>
+                  <ul className="default-list">
+                    <span className="default-span">Disabled</span>
+                    <li>
+                      <a href="-" className="link link__arrow disable">
+                        Home Sales
+                        <LinkArrow />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
               <div className="default-block">
-                <div className="default-title">Empty</div>
+                <div className="default-title">Inputs</div>
+                <div className="default-block__wrap">
+                  <ul className="default-list">
+                    <span className="default-span">Default</span>
+                    <li>
+                      <div className="input required" name="name">
+                        <label htmlFor="1">Name</label>
+                        <input id="1" type="text" placeholder="Name" />
+                      </div>
+                    </li>
+                  </ul>
+                  <ul className="default-list">
+                    <span className="default-span">Success</span>
+                    <li>
+                      <div className="input required success" name="email">
+                        <label htmlFor="2">Email</label>
+                        <input id="2" type="text" placeholder="Email" />
+                      </div>
+                    </li>
+                  </ul>
+                  <ul className="default-list">
+                    <span className="default-span">Error</span>
+                    <li>
+                      <div className="input required error" name="phone">
+                        <label htmlFor="3">Phone</label>
+                        <input id="3" type="text" placeholder="Phone" />
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="default-block">
+                <div className="default-title">Radio buttons</div>
+                <div className="default-block__wrap">
+                  <ul className="default-list">
+                    <span className="default-span">Active</span>
+                    <li>
+                      <div className="radio">
+                        <input id="radioFirst" type="radio" />
+                        <label htmlFor="radioFirst">Label</label>
+                      </div>
+                    </li>
+                  </ul>
+                  <ul className="default-list">
+                    <span className="default-span">Disabled</span>
+                    <li>
+                      <div className="radio disabled">
+                        <input id="radioFirst" type="radio" />
+                        <label htmlFor="radioFirst">Label</label>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="default-block">
+                <div className="default-title">Labels</div>
+                <div className="default-block__wrap">
+                  <ul className="default-list">
+                    <span className="default-span">Active Listings</span>
+                    <li>
+                      <span className="label active">Active</span>
+                    </li>
+                  </ul>
+                  <ul className="default-list">
+                    <span className="default-span">Sold Listings</span>
+                    <li>
+                      <span className="label sold">Sold listings</span>
+                    </li>
+                  </ul>
+                  <ul className="default-list">
+                    <span className="default-span">Pending Listings</span>
+                    <li>
+                      <span className="label pending">Pending</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="default-block">
+                <div className="default-title">Pagination</div>
+                <div className="default-block__wrap">
+                  <ul className="default-list">
+                    <li>
+                      <ul className="pagination">
+                        <li>
+                          <a href="-">
+                            <span>&laquo;</span>
+                          </a>
+                        </li>
+                        <li className="active">
+                          <a href="-">1</a>
+                        </li>
+                        <li>
+                          <a href="-">2</a>
+                        </li>
+                        <li>
+                          <a href="-">3</a>
+                        </li>
+                        <li>
+                          <span>...</span>
+                        </li>
+                        <li>
+                          <a href="-">32</a>
+                        </li>
+                        <li>
+                          <a href="-">
+                            <span>&raquo;</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
@@ -60,3 +232,6 @@ function Default() {
 }
 
 export default Default
+/* <div className="default-block">
+  <div className="default-title">Empty</div>
+</div> */
